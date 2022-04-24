e811a32fc2f44fd20d5619d64774b2fa3c1e6604
Xiaoling <sxlwork@outlook.jp>
2022-04-24 14:17:23 +0900

    12.1 over

--- src/NamedRunnable.java
+++ src/NamedRunnable.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class NamedRunnable {
+public class NamedRunnable implements Runnable{
+    private String name;
+
+    public NamedRunnable(String name){
+        this.name = name;
+    }
+
+    public void run(){
+        System.out.println("The runnable thread is called :" + name);
+    }
+}
+
+class TestNamedRunnable{
+    public static void main(String[] args) {
+        NamedRunnable nr = new NamedRunnable("RunningLOL");
+        Thread thr = new Thread(nr);
+        thr.start();
+
+        // Runnable is a functional interface.
+        // lambda expression
+        Thread lambdaR = new Thread(() ->
+                System.out.println("Greeting from lambda Runnable"));
+        lambdaR.start();
+    }
 }
